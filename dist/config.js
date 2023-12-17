cf216c604993a088a5fd332725901e1107016fdb
Timur <komolov.timurka@mail.ru>
2023-12-18 01:45:03 +0300

    Update config.js

--- dist/config.js
+++ dist/config.js
@@ -3,7 +3,7 @@ export class Config {
         var _a, _b, _c, _d;
         this.host = (_a = process.env.HOST) !== null && _a !== void 0 ? _a : "localhost";
         this.user = (_b = process.env.USER) !== null && _b !== void 0 ? _b : "root";
-        this.password = (_c = process.env.PASSWORD) !== null && _c !== void 0 ? _c : "Ser_13_volk";
+        this.password = (_c = process.env.PASSWORD) !== null && _c !== void 0 ? _c : "BasePassword";
         this.database = (_d = process.env.DATABASE) !== null && _d !== void 0 ? _d : "pictureme";
     }
 }
